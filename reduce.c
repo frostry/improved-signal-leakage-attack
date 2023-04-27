002c8de3216cb24dd282d5bfcd1761d05f0e6510
hungrydonkey <35836426+hungrydonkey@users.noreply.github.com>
2023-04-27 23:48:05 +0800

    Update reduce.c

--- reduce.c
+++ reduce.c
@@ -8,6 +8,6 @@ uint32_t montgomery_reduce(uint64_t a)
   u &= ((1<<NEWHOPE_RLOG)-1);
   u *= NEWHOPE_Q;
   a = a + u;
-  return a >> NEWHOPE_RLOG;    // here is not correct, lack the condition whether a > q ? 
+  return a >> NEWHOPE_RLOG;    // It's not correct, lacking the condition whether a > q ? 
   //return a >= NEWHOPE_Q ? (a - NEWHOPE_Q) : a;
 }
